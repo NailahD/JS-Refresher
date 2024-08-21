bfb46f637f2ca80d63eba8f364e0011b97cf0b42
Nailah Davis <ndavis40@emich.edu>
2024-08-20 23:51:33 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const firstName = "Nailah";
+const lastName = "Davis";
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+
+let population = 620376;
+console.log(population);
+console.log(population * 3);
+
+let javaScriptIsCool = true;
+
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+for (let i = 0; i <= 4; i++) {
+  console.log(i);
+}
